355281e1eb4f6e47146baedb8ba46082e7842632
canab <canab.ua@gmail.com>
2014-09-29 23:21:10 +0300

    ready for examples

---
+++ SampleProject/Assets/Examples/FractalTree.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using FlashBundles;
+using Flunity;
+
+namespace Examples
+{
+	public class FractalTree : LiveReloadableScene
+	{
+		protected override void CreateScene()
+		{
+			stage.root.AddChild(new MovieClip(SceneBundle.McScene1));
+		}
+	}
+}
+
